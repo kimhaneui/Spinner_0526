487d200f0ee05702296c345de600150a5f22da97
kimhaneui <kimhaneui2@naver.com>
2020-05-26 16:36:08 +0900

    실제 직군 이름과 이미지 반영  - setImageResource로 그림 지정 가능

--- app/src/main/java/com/example/spinner_0526/MainActivity.java
+++ app/src/main/java/com/example/spinner_0526/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.spinner_0526.adapter.OccapationAdapter;
 import com.example.spinner_0526.databinding.ActivityMainBinding;
+import com.example.spinner_0526.datas.Occupation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
+
+    List<Occupation> occupations = new ArrayList<>();
+    OccapationAdapter osa;
 
     ActivityMainBinding binding;
 
@@ -26,6 +34,18 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+        addOccupation();
+        osa = new OccapationAdapter(mContext,R.layout.occupation_spinner_list_item,occupations);
+        binding.occSpiner.setAdapter(osa);
 
     }
+
+    void addOccupation(){
+
+        occupations.add(new Occupation("안드로이드",R.drawable.android));
+        occupations.add(new Occupation("ios",R.drawable.ios_icon));
+        occupations.add(new Occupation("Web",R.drawable.web));
+        occupations.add(new Occupation("Server",R.drawable.server));
+    }
+
 }
--- app/src/main/java/com/example/spinner_0526/adapter/OccapationAdapter.java
+++ app/src/main/java/com/example/spinner_0526/adapter/OccapationAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,13 +16,13 @@ import com.example.spinner_0526.datas.Occupation;
 
 import java.util.List;
 
-public class occapationAdapter extends ArrayAdapter<Occupation> {
+public class OccapationAdapter extends ArrayAdapter<Occupation> {
 
     Context mContext;
     List<Occupation> mList;
     LayoutInflater inf;
 
-    public occapationAdapter(@NonNull Context context, int resource, @NonNull List<Occupation> objects) {
+    public OccapationAdapter(@NonNull Context context, int resource, @NonNull List<Occupation> objects) {
         super(context, resource, objects);
     }
 
@@ -31,6 +33,14 @@ public class occapationAdapter extends ArrayAdapter<Occupation> {
         if(row==null){
             row = inf.inflate(R.layout.occupation_spinner_list_item,null);
         }
+        ImageView icon = row.findViewById(R.id.icon);
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+
+        Occupation data = mList.get(position);
+
+        nameTxt.setText(data.getName());
+
+        icon.setImageResource(data.getImageId());
         return row;
     }
 }
